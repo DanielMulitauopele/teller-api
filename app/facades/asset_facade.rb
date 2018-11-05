97744607bf5e081a80cbc6021023442d916c08ba
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-11-05 15:06:52 -0700

    removes max supply validation, able to create assets using coin cap data, passes tests

--- app/facades/asset_facade.rb
+++ app/facades/asset_facade.rb
@@ -1,7 +1,16 @@
 class AssetFacade
   def assets
     cc_service.get_assets.map do |asset_details|
-      Asset.new(asset_details)
+      Asset.create!(
+        name: asset_details[:name],
+        rank: asset_details[:rank],
+        symbol: asset_details[:symbol],
+        supply: asset_details[:supply],
+        max_supply: asset_details[:maxSupply],
+        market_cap_usd: asset_details[:marketCapUsd],
+        price_usd: asset_details[:priceUsd],
+        percent_change_24_hr: asset_details[:changePercent24Hr]
+      )
     end
   end
 
--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,3 +1,6 @@
 class Asset < ApplicationRecord
-
+  validates_presence_of :name,
+                        :supply,
+                        :market_cap_usd,
+                        :price_usd
 end
