35e4574d3abe5c1a094f3454d8c608b17b6b7209
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-11-05 10:06:32 -0700

    gets test to pass, connection is working, need to work on refactoring out the connection from the manipulation of the data

--- app/services/coin_cap_api_interface.rb
+++ app/services/coin_cap_api_interface.rb
@@ -1,5 +1,6 @@
 class CoinCapApiInterface
   def get_assets
-
+    conn =  Faraday.new(url: ENV['COINCAP_PATH'])
+    response = conn.get('/v2/assets')
   end
 end
--- spec/services/coin_cap_api_interface_spec.rb
+++ spec/services/coin_cap_api_interface_spec.rb
@@ -9,9 +9,10 @@ describe 'CoinCapApiInterface' do
     coincap = CoinCapApiInterface.new
 
     response = coincap.get_assets
+    parsed = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response).to be_successful
-    expect(response).to be_a(Hash)
-    expect(response[:data]).to be_an(Array)
+    expect(response.status).to eq(200)
+    expect(parsed).to be_a(Hash)
+    expect(parsed[:data]).to be_an(Array)
   end
 end
