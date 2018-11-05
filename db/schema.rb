f90133cad468944715f1a28040a767796c116b9a
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-11-05 13:49:57 -0700

    adds assets migration, may need to adjust later

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_11_05_204814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "assets", force: :cascade do |t|
+    t.string "name"
+    t.integer "rank"
+    t.string "symbol"
+    t.float "supply"
+    t.float "max_supply"
+    t.float "market_cap_usd"
+    t.float "price_usd"
+    t.string "percent_change_24_hr"
+  end
 
 end
