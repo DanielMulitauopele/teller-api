a57f29cfc0bb21ffdc272b155f53339f95e7898d
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-23 11:56:57 -0700

    Test case-insensitive uniqueness of User email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,5 @@ RSpec.describe User, type: :model do
   it {should validate_presence_of :password}
   it {should validate_presence_of :password_confirmation}
   it {should have_secure_password}
-  it {should validate_length_of(:password).is_at_least(10)}
+  it {should validate_uniqueness_of(:email).ignoring_case_sensitivity}
 end
