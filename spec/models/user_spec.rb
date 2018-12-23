05af7e276e085fb9e9cc34fb307a9e929a6131c0
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-23 11:25:22 -0700

    Test for User password to be secure and atleast 10 characters

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe User, type: :model do
   it {should validate_presence_of :email}
   it {should validate_presence_of :password}
   it {should validate_presence_of :password_confirmation}
+  it {should have_secure_password}
+  it {should validate_length_of(:password).is_at_least(10)}
 end
