3b7685e6d09b812d18de34ec3886db3942d3994d
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-23 12:02:03 -0700

    Re-add test for User password to be atleast 10 characters

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   it {should validate_presence_of :email}
   it {should validate_presence_of :password}
   it {should validate_presence_of :password_confirmation}
+  it {should validate_length_of(:password).is_at_least(10)}
   it {should have_secure_password}
   it {should validate_uniqueness_of(:email).ignoring_case_sensitivity}
 end
