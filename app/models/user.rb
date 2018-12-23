5c12649e3d1e5ba5503c2d0f0f873b81db59dfd9
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-23 11:57:36 -0700

    Validate case-insensitive uniqueness of User email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates_presence_of :email, :password, :password_confirmation
   has_secure_password
   validates_length_of :password, minimum: 10
+  validates :email, uniqueness: {case_sensitive: false}
 end
