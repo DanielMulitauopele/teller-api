629ec7fdcd59950dffacf67130be491ccf1e4313
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-24 12:38:44 -0800

    Add before action to authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user
+    attr_reader :current_user
+
+  private
+  def authenticate_user
+    @current_user = Authorization.call(request.headers).result
+    render json: {error: 'Not authorized to make this request'}, status: 401 unless @current_user
+  end
+
 end
