70a782a855ac2bc51aa2a6c04309a742db2b0a84
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-27 02:01:20 -0700

    Add sad path & status code for user creation to make tests pass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,13 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user, :verify_authenticity_token
 
   def create
-    User.create(user_params)
-    authenticate
+    user = User.create(user_params)
+    if user.save
+      authenticate
+    else
+      render json: {error: "Unfortunately, your account could not be created. Please make sure your password is longer than 10 characters."},
+             status: 400
+    end
   end
 
   private
@@ -11,7 +16,8 @@ class UsersController < ApplicationController
     auth_new_user = Authentication.call(user_params[:email], user_params[:password])
 
     if auth_new_user.success?
-      render json: {teller_api_token: auth_new_user.result}
+      render json: {teller_api_token: auth_new_user.result},
+             status: 201
     else
       render json: {error: auth_new_user.errors}, status: :unauthorized
     end
