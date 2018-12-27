62ac6cd6442f49458624080c791d609c8d552665
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-26 23:33:35 -0700

    Update asset request spec with user auth reqs

--- spec/requests/asset_request_spec.rb
+++ spec/requests/asset_request_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 describe 'Assets API' do
+  before do
+    user_params = { email: "groot@email.com",
+                    password: "IamGrootIamGroot",
+                    password_confirmation: "IamGrootIamGroot"
+                  }
+    post '/users', params: { user: user_params }
+
+    @user_teller_token = JSON.parse(response.body)["teller_api_token"]
+  end
+
   it "sends a list of assets" do
-    get '/api/v1/assets'
+    get '/api/v1/assets', headers: {"Authorization" => "#{@user_teller_token}"}
 
     expect(response).to be_successful
+    expect(response).to have_http_status(200)
   end
 end
