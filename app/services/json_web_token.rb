cbbd4e02e73df11820e8f364841dc09579aeac49
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-27 16:21:20 -0700

    Nest teller secret token

--- app/services/json_web_token.rb
+++ app/services/json_web_token.rb
@@ -3,12 +3,12 @@ class JsonWebToken
     # creates a unique token to authenticate the user based on user ID, the unique base key of our Rails app, & expiration
     def encrypt(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.credentials.teller_secret_key_base)
+      JWT.encode(payload, Rails.application.credentials.teller[:teller_secret_key_base])
     end
 
     # uses our app's secret key to decode the token & check validity of each request
     def decrypt(token)
-      body = JWT.decode(token, Rails.application.credentials.teller_secret_key_base)[0]
+      body = JWT.decode(token, Rails.application.credentials.teller[:teller_secret_key_base])[0]
       HashWithIndifferentAccess.new(body) # HashWithIndifferentAccess gives access to hash as a symbol or string
     rescue # throws an exception
       nil
