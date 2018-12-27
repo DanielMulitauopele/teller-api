3bd2dbc348f99ed3b948b730ff13c06c3889d96d
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-12-27 00:55:02 -0700

    Update transaction request spec with user auth reqs & sad path

--- spec/requests/transaction_request_spec.rb
+++ spec/requests/transaction_request_spec.rb
@@ -1,9 +1,47 @@
 require 'rails_helper'
 
 describe 'Transactions API' do
-  it "sends a list of most recent transactions" do
-    get '/api/v1/transactions'
+  describe 'valid request with auth' do
+    before do
+      user_params = { email: "drax@email.com",
+                      password: "DraxTheDestroyer",
+                      password_confirmation: "DraxTheDestroyer"
+                    }
+      post '/users', params: { user: user_params }
+      @user_teller_token = JSON.parse(response.body)["teller_api_token"]
+    end
 
-    expect(response).to be_successful
+    it 'sends a list of transactions' do
+      get '/api/v1/transactions', headers: {"Authorization" => "#{@user_teller_token}"}
+      transactions = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+
+      expect(transactions.class).to eq(Array)
+      expect(transactions[0].class).to eq(Hash)
+    end
+  end
+  describe 'invalid request' do
+    describe 'without auth headers' do
+      it 'sends an error message' do
+        get '/api/v1/transactions'
+        error = JSON.parse(response.body)
+
+        expect(response).to_not be_successful
+        expect(response).to have_http_status(401)
+        expect(error["error"]).to eq("Not authorized to make this request")
+      end
+    end
+    describe 'without invalid credentials' do
+      it 'sends an error message' do
+        get '/api/v1/transactions', headers: {"Authorization" => "INVALID"}
+        error = JSON.parse(response.body)
+
+        expect(response).to_not be_successful
+        expect(response).to have_http_status(401)
+        expect(error["error"]).to eq("Not authorized to make this request")
+      end
+    end
   end
 end
