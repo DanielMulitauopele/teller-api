9860e6ab38de086fb6c1f073a7fc8eaff9391529
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-12-31 14:32:12 -0700

    Adds simple request spec for history endpoints, adjust routes slightly, putting in slugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/assets', to: 'assets#index'
-      get '/transactions', to: 'transactions#index'
-      get '/exchanges', to: 'exchanges#index'
+      resources :assets, only: [:index]
+      resources :assets, only: [:show] do
+        resources :history, only: [:index]
+      end
+      
+      resources :transactions, only: [:index]
+      resources :exchanges, only: [:index]
     end
   end
   resources :authentications, only: [:create]
