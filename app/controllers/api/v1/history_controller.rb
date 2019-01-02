b85def5b5212edde0fa8077c945edbf5a4d3eafc
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-01-01 20:41:18 -0700

    Adds error handling for when user does not include interval

--- app/controllers/api/v1/history_controller.rb
+++ app/controllers/api/v1/history_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::HistoryController < ApplicationController
   def index
-    render json: HistoryFacade.new.history(params[:asset_id], params[:interval])
+    if params[:asset_id] && params[:interval]
+      render json: HistoryFacade.new.history(params[:asset_id], params[:interval])
+    elsif params[:asset_id]
+      render json: {error: "Please choose a valid interval (m1, m5, m15, m30, h1, h2, h6, h12, d1) to examine history."}
+    else
+      render json: {error: "Please choose a coin to examine."}
+    end
   end
 end
