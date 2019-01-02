29a38e8ccc4eda5559881be998286dde4a97eb79
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-01-02 14:22:32 -0700

    Adds skip before actions to endpoints, users should be able to access all endpoints except favorites and notes

--- app/controllers/api/v1/exchanges_controller.rb
+++ app/controllers/api/v1/exchanges_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::ExchangesController < ApplicationController
+  skip_before_action :authenticate_user, :verify_authenticity_token
+
   def index
     render json: ExchangeFacade.new.exchanges
   end
