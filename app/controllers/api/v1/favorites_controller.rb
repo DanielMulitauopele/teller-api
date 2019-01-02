ae962e0442e95faaa5e75611afe961def076c3d8
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-01-02 16:30:16 -0700

    Adds favorite request spec, favorites index route, and favorites controller

---
+++ app/controllers/api/v1/favorites_controller.rb
@@ -0,0 +1,3 @@
+class Api::V1::FavoritesController < ApplicationController
+  
+end
---
+++ spec/requests/favorite_request_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe 'Favorites API' do
+  describe 'valid request with auth' do
+    before do
+      user_params = { email: "groot@email.com",
+                      password: "IamGrootIamGroot",
+                      password_confirmation: "IamGrootIamGroot"
+                    }
+      post '/users', params: { user: user_params }
+      @user_teller_token = JSON.parse(response.body)["teller_api_token"]
+    end
+
+    it "sends the user's favorites" do
+      get '/api/v1/favorites', headers: {"Authorization" => "#{@user_teller_token}"}
+      favorites = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+
+      expect(response.headers["Accept"]).to eq("application/json")
+      expect(response.headers["Content-Type"]).to eq("application/json")
+
+      expect(favorites.class).to eq(Array)
+      expect(favorites[0].class).to eq(Hash)
+
+      expect(favorites[0]).to have_key("name")
+      expect(favorites[0]).to have_key("price_usd")
+      expect(favorites[0]).to have_key("percent_change_24_hr")
+
+      expect(favorites[0]["name"]).not_to be(nil)
+      expect(favorites[0]["price_usd"]).not_to be(nil)
+      expect(favorites[0]["percent_change_24_hr"]).not_to be(nil)
+    end
+  end
+end
