d75b7e84ed1635c781726cad7d187daaffed28d6
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-01-02 16:19:54 -0700

    Adds favorite table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_23_174007) do
+ActiveRecord::Schema.define(version: 2019_01_02_224047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,9 +26,18 @@ ActiveRecord::Schema.define(version: 2018_12_23_174007) do
     t.string "percent_change_24_hr"
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.string "name"
+    t.float "price_usd"
+    t.string "percent_change_24_hr"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
   end
 
+  add_foreign_key "favorites", "users"
 end
