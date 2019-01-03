bcb873fc77dc6bd46e4475c5f43f901a29c0c661
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-01-02 22:14:13 -0700

    Adds request test for notes

---
+++ spec/requests/note_request_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe 'Notes API' do
+  describe 'valid request with auth' do
+    before (:each) do
+      user_params = { email: "rocket@email.com",
+                      password: "IllgetthatArm",
+                      password_confirmation: "IllgetthatArm"
+                    }
+      post '/users', params: { user: user_params }
+
+      @user = User.last
+      @note = @user.notes.create!(title: "Oh Ill get that arm", text: "Winter Soldier? More like Winter Whiner.")
+      @user_teller_token = JSON.parse(response.body)["teller_api_token"]
+    end
+
+    it "lists the user's notes" do
+      get '/api/v1/notes', headers: {"Authorization" => "#{@user_teller_token}"}
+      notes = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+
+      expect(response.headers["Accept"]).to eq("application/json")
+      expect(response.headers["Content-Type"]).to eq("application/json")
+
+      expect(notes.class).to eq(Array)
+      expect(notes[0].class).to eq(Hash)
+
+      expect(notes[0]).to have_key("title")
+      expect(notes[0]).to have_key("text")
+
+      expect(notes[0]["title"]).not_to be(nil)
+      expect(notes[0]["text"]).not_to be(nil)
+    end
+  end
+end 
