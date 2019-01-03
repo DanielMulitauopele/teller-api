0a30aadd5b492207dc3857c115c13fd9c91e0268
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-01-02 22:08:49 -0700

    Adds rails migration for notes

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema.define(version: 2019_01_03_040336) do
+ActiveRecord::Schema.define(version: 2019_01_03_050724) do
   enable_extension "plpgsql"
 
   create_table "favorites", force: :cascade do |t|
@@ -9,10 +9,18 @@ ActiveRecord::Schema.define(version: 2019_01_03_040336) do
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
   end
 
   add_foreign_key "favorites", "users"
+  add_foreign_key "notes", "users"
 end
